ce122460df103d354fe681f9659edccbac76f1f2
gitrepo-dev <jaysean.developer@gmail.com>
2022-08-27 11:07:40 +0530

    job 2

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,7 +1,7 @@
 // config dynamodb
 const {DynamoDBClient} = require("@aws-sdk/client-dynamodb");
 const client = new DynamoDBClient({
-    // commit when deploy on aws config
+    // commit when deploy on aws
     // region: "localhost",
     // accessKeyId: "aws_key_id",
     // secretAccessKeyId: "aws_key_secret",
